f5606460ca59bac22e7f20580f6eb7a4bf824a97
RYANLIL <ryan.lildhar@gmail.com>
2018-12-23 20:15:40 -0500

    seperating out componets and function on blur

--- src/containers/Series/series.js
+++ src/containers/Series/series.js
@@ -1,2 +1,64 @@
 import React, { Component } from 'react';
+import SeriesList from '../../components/SeriesList/seriesList'
 
+class Series extends Component {
+    state = {
+        series: [],
+        pop:{}
+      }
+    
+      async componentDidMount() {
+        // try {
+        //   const res = await fetch('http://api.tvmaze.com/search/shows?q=Vikings');
+        //   if(!res.ok){
+        //     throw Error(res.statusText);
+        //   }
+        //   const data = await res.json();
+        //   this.setState({series: data});
+    
+        // } catch (err){
+        //   console.error(err);
+        // }
+    
+        // // Callback hell fetch
+        // fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+        // .then(response =>  response.json())
+        // .then( json => this.setState({series: json}));
+    
+        // const series = ["Vikings","Game of Thrones"];
+        // setTimeout(()=> {
+        //   this.setState({series});
+        // },2000);
+      }
+      onSeriesInputChange = async e => {
+          const {value} = e.target; // value = e.target.value
+        try {
+            const res = await fetch(`http://api.tvmaze.com/search/shows?q=${value}`);
+            if(!res.ok){
+              throw Error(res.statusText);
+            }
+            const data = await res.json();
+            this.setState({series: data});
+      
+          } catch (err){
+            console.error(err);
+          }
+      }
+
+      render() {
+        return (
+            <div>
+                The length of series array - {this.state.series.length}
+                <div>
+                    <input type="text" onBlur={this.onSeriesInputChange} />
+                </div>
+                <SeriesList list={this.state.series} />                
+            </div>
+
+            
+        )
+
+      }
+    
+}
+export default Series;
